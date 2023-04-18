d6980ffe978eaf85985fc220150576b8829a1972
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2023-04-18 18:59:06 +0200

    Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <footer aria-label="Site Footer" className="bg-gray-50">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="flex justify-center sm:justify-start">
+            <Link href="/">
+              <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                mohi.to
+              </h1>
+            </Link>
+          </div>
+          <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+            Created by: Adam Łukasiewicz
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
