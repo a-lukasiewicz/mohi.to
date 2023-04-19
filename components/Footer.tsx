2eb7b80dc5b4ed0c7184115c1e839e528b2183fe
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2023-04-19 17:32:32 +0200

    update Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer aria-label="Site Footer" className="bg-gray-50">
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="flex flex-col md:flex-row justify-center sm:items-center sm:justify-between">
           <div className="flex justify-center sm:justify-start">
             <Link href="/">
               <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
