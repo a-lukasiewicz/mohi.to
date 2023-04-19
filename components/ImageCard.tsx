cbeca1f31ddfba1fb62d3de53a6fa0bce151c2e6
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2023-04-19 17:31:54 +0200

    update ImageCard component

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface ImageCardType {
+  id: string
   title: string
   imageURL: string
 }
 
-const ImageCard = ({ title, imageURL }: ImageCardType) => {
+const ImageCard = ({ id, title, imageURL }: ImageCardType) => {
   return (
-    <>
+    <Link href={`/photo/${id}`}>
       <Image
         src={imageURL}
         alt={title}
@@ -15,7 +17,7 @@ const ImageCard = ({ title, imageURL }: ImageCardType) => {
         height={500}
         className="rounded hover:scale-105 hover:cursor-pointer transition"
       />
-    </>
+    </Link>
   )
 }
 
