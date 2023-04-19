493e33fe1839ae8684f7932c4c85dfa22b2ca54e
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2023-04-19 17:31:20 +0200

    update layout styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col h-screen">
         <Header />
-        <main>{children}</main>
+        <main className="grow">{children}</main>
         <Footer />
       </body>
     </html>
