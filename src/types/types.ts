c624329f9864da11ecb56d3cd9c1bc4fcafdba80
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2023-04-20 12:12:53 +0200

    add custom useDebounce hook

---
+++ src/types/types.ts
@@ -0,0 +1,26 @@
+export interface Image {
+  id: string
+  width: number
+  height: number
+  url: string
+  photographer: string
+  photographer_url: string
+  photographer_id: number
+  avg_color: string
+  src: {
+    original: string
+    large2x: string
+    large: string
+    medium: string
+    small: string
+    portrait: string
+    landscape: string
+    tiny: string
+  }
+  liked: boolean
+  alt: string
+}
+
+export enum LocalStorageKeys {
+  FAVORITES = 'favorites'
+}
