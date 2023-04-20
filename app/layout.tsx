8f1f611fd6c06389f3b54801c0baf2d059349d5c
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2023-04-20 10:11:43 +0200

    reorder imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/Header'
-import './globals.css'
 import Footer from '@/components/Footer'
+import './globals.css'
 
 export const metadata = {
   title: 'Adam Łukasiewicz - mohi.to',
