66c17e41a8e272ae09f327ab9b4017cf3215eea8
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2023-04-22 13:07:42 +0200

    move components to src/

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,16 +1,21 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
 
 const Pagination = () => {
+  const [currentPage, setCurrentPage] = useState(0)
+
   return (
     <div className="inline-flex items-center justify-center gap-1">
-      <Link href="#" className="paginationArrow">
+      <Link href={`/page/${currentPage}`} className="paginationArrow">
         <Image src="/prevPage.svg" alt="Prev Page" width={15} height={15} />
       </Link>
       <div className="px-4">
         <h3>Page 1</h3>
       </div>
-      <Link href="#" className="paginationArrow">
+      <Link href={`/page/${currentPage + 1}`} className="paginationArrow">
         <Image src="/nextPage.svg" alt="Next Page" width={15} height={15} />
       </Link>
     </div>
