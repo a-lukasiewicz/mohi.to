29c47364250f062c1a2d10ed7253ebf27213b2c1
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2023-04-23 14:26:14 +0200

    fix localStorage undefined value for vercel

--- src/helpers/localStorage.ts
+++ src/helpers/localStorage.ts
@@ -1,5 +1,5 @@
 export const getLocalStorageData = (key: string) => {
-  const elements: any = localStorage.getItem(key)
+  const elements: any = window.localStorage.getItem(key)
 
   if (!elements) return []
   return JSON.parse(elements)
@@ -10,21 +10,21 @@ export const checkLocalStorage = (key: string, id: string) => {
 }
 
 export const clearLocalStorage = (key: string) => {
-  localStorage.setItem(key, JSON.stringify([]))
+  window.localStorage.setItem(key, JSON.stringify([]))
 }
 
 export const setLocalStorage = (key: string, id: string) => {
   const data: string[] = getLocalStorageData(key)
 
   if (!data) {
-    return localStorage.setItem(key, JSON.stringify([id] as string[]))
+    return window.localStorage.setItem(key, JSON.stringify([id] as string[]))
   }
 
   if (data.includes(id)) {
     return
   }
 
-  localStorage.setItem(key, JSON.stringify([...data, id]))
+  window.localStorage.setItem(key, JSON.stringify([...data, id]))
 }
 
 export const removeFromLocalStorage = (key: string, id: string) => {
@@ -35,5 +35,5 @@ export const removeFromLocalStorage = (key: string, id: string) => {
   }
 
   const filteredData = data.filter(fav => fav !== id)
-  localStorage.setItem(key, JSON.stringify(filteredData))
+  window.localStorage.setItem(key, JSON.stringify(filteredData))
 }
